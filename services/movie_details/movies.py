29194b58fcee7a733d0ae0a9a5549e83521a492e
stanajin <natu.neeraj@gmail.com>
2019-03-28 14:40:09 +0000

    movies service

--- services/movie_details/movies.py
+++ services/movie_details/movies.py
@@ -1,20 +1,27 @@
 from flask import Flask
 import json
 import pandas as pd
-import numpy as np
+from flask import request
+
 
 
 app = Flask(__name__)
 
 
 
-@app.route("/movies")
-def details():
-    details = pd.read_csv('movie_details.csv');    
-    return details.to_string(index=False)
-
+@app.route('/movies', methods=['GET']) 
+def movie_details():
+    title = request.args.get('title');
+    movies = pd.read_csv('movie_details.csv');
+    
+    if (title is not None):
+        movies.set_index('title',inplace=True)
+        response = movies.loc[title,:];
+        return str(response.to_dict());
+    else :
+        return movies.to_string(index=False)
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=5005)
+    app.run(host='0.0.0.0',port=5000)
 
