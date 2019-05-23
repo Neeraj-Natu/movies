598170bf28a147218707d8bae02fbb6dd1c7bc0b
stanajin <natu.neeraj@gmail.com>
2019-05-23 23:40:10 +0530

    adding more services

---
+++ micro-services/services/popularity/popularity.py
@@ -0,0 +1,31 @@
+from flask import Flask
+import json
+import pandas as pd
+from flask import request
+
+
+
+app = Flask(__name__)
+
+
+@app.route('/') 
+def rediness_check():
+    
+    return "200";
+
+@app.route('/popularity', methods=['GET']) 
+def movie_details():
+    title = request.args.get('title');
+    movies = pd.read_csv('movie_details.csv');
+    
+    if (title is not None):
+        movies.set_index('title',inplace=True)
+        response = movies.loc[title,:];
+        return json.dumps(response.to_dict());
+    else :
+        return "no title found"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',port=80)
+
